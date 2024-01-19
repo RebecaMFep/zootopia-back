a8ea8a7173446de7e14a2014e96a551f60a46c89
RebecaMFep <martinezfeliperebeca@gmail.com>
2024-01-19 12:00:11 +0100

    created SpecieService.java

---
+++ src/main/java/dev/team2/zoopoli/Services/SpecieService.java
@@ -0,0 +1,43 @@
+package dev.team2.zoopoli.Services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import dev.team2.zoopoli.Models.Specie;
+import dev.team2.zoopoli.Repositories.SpecieRepository;
+
+
+
+
+
+@Service
+
+public class SpecieService {
+
+    private SpecieRepository repository;
+
+    public SpecieService(SpecieRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Specie> getAll() {
+        return repository.findAll();
+    }
+
+    public Specie getOne(Long id) {
+        Specie Specie = repository.findById(id).orElse(null);
+        return Specie;
+    }
+
+    public Specie save(Specie newSpecie) {
+        Specie SpecieSaved = repository.save(newSpecie);
+        return SpecieSaved;
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+    
+}
+    
